a96aa35a68b7313b85ed341bd78dbbb88e3da2a8
Satvik Gosai <136249990+satvikgosai@users.noreply.github.com>
2025-04-05 12:36:59 +0530

    add: colour and refresh support

--- typer.py
+++ typer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import collections
+import functools
 import itertools
 import os
 import random
+import signal
 import string
 import sys
 import termios
+import textwrap
 import time
 import tty
 
@@ -17,6 +20,8 @@ with open(WORDS_PATH, 'r') as f:
 ascii_letters = set(string.ascii_letters)
 
 usable_words = list(set(word.lower() for word in words if not (set(word) - ascii_letters)))
+
+printable = set(string.printable)
 
 codes = {
     'clear': '\x1b[H\x1b[2J\x1b[3J',
@@ -34,20 +39,36 @@ def pixel(char, r, g, b, back=False):
 
 
 class Sentence:
+    _width = 100
+    _pad = ' '
 
     def __init__(self, length=15):
         self._remain = collections.deque(' '.join(random.choices(usable_words, k=length)))
         self._len = len(self._remain)
-        self._success = []
+        self._success = ''
         self._errors = []
+        self.update_width()
 
     def __repr__(self):
-        errors, remain, u = self._errors, self._remain, ''
-        if errors:
-            *errors, u = errors
-        elif remain:
-            u, *remain = remain
-        return ''.join(itertools.chain(self._success, errors, [codes['underline'].format(char=u)], remain))
+        text = ''.join(itertools.chain(self._success, self._errors, self._remain))
+        wrapped = textwrap.wrap(text, width=self._width, expand_tabs=False, drop_whitespace=False)
+        s_cur = len(self._success)
+        e_cur = s_cur + len(self._errors)
+        u = s_cur if s_cur == e_cur else e_cur - 1
+        final, count = ['\n\n'], 0
+        for sen in wrapped:
+            final.append(self._pad)
+            for w in sen:
+                if count < s_cur:
+                    cur = pixel(w, 100, 102, 105)
+                elif count < e_cur:
+                    cur = pixel(w, 217, 4, 82)
+                else:
+                    cur = w
+                final.append(cur if count != u else codes['underline'].format(char=cur))
+                count += 1
+            final.append('\n')
+        return ''.join(final)
 
     def __len__(self):
         return self._len
@@ -59,8 +80,14 @@ class Sentence:
     def head(self):
         return self._remain[0]
 
+    @classmethod
+    def update_width(cls):
+        term_len = os.get_terminal_size().columns
+        cls._width = int((term_len * 0.8))
+        cls._pad = ' ' * int((term_len * 0.2) // 2)
+
     def success(self):
-        self._success.append(pixel(self._remain.popleft(), 100, 102, 105))
+        self._success += self._remain.popleft()
 
     @property
     def errors(self):
@@ -70,43 +97,54 @@ class Sentence:
         self._errors.pop()
 
     def append_error(self, val):
-        self._errors.append(pixel(val, 217, 4, 82))
+        self._errors.append(val)
 
 
-def stdout(*args):
-    print(*args, sep='', end='', flush=True)
+def refresh(out):
+    t0 = time.time()
+    out_repr = repr(out)
+    t1 = time.time() - t0
+    sys.stdout.write(codes['clear'])
+    sys.stdout.write(out_repr)
+    sys.stdout.flush()
+    print(f'repr: {t1}', f'raw: {out_repr!r}')
+
+
+def resize(out, *_):
+    Sentence.update_width()
+    refresh(out)
 
 
 def main():
     stdin_fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(stdin_fd)
+    try:
+        old_settings = termios.tcgetattr(stdin_fd)
+    except termios.error as e:
+        print(f'This terminal is not supported: {e}')
+        exit(0)
     try:
         while not (inp := input('Enter word amount: ') or '15').isdigit() or not (num := int(inp)): pass
         sentence = Sentence(num)
         tty.setcbreak(stdin_fd)
-        stdout(codes['cursor_invisible'])
-        stdout(codes['clear'])
-        stdout(sentence)
-        count_raw = 0
-        total_time = 0
+        print(codes['cursor_invisible'])
+        refresh(sentence)
+        signal.signal(signal.SIGWINCH, functools.partial(resize, sentence))
+        count_raw = total_time = 0
         while sentence:
             t0 = time.time()
             ch = os.read(stdin_fd, 80).decode("utf-8", errors="ignore")
             total_time += (time.time() - t0) if count_raw else 0
-            if sentence.errors:
-                if ch == codes['back']:
+            if ch == codes['back']:
+                if sentence.errors:
                     sentence.pop_error()
-                elif ch.isalnum():
-                    count_raw += 1
+                    refresh(sentence)
+            elif ch in printable:
+                if sentence.errors or sentence.head != ch:
                     sentence.append_error(ch)
-            elif sentence.head == ch:
+                else:
+                    sentence.success()
                 count_raw += 1
-                sentence.success()
-            elif ch.isalnum():
-                count_raw += 1
-                sentence.append_error(ch)
-            stdout(codes['clear'])
-            stdout(sentence)
+                refresh(sentence)
         total_time /= 60
         print('\nRWPM: ', rwpm := round(count_raw / 5 / total_time, 1))
         print('Accuracy: ', acc := round(len(sentence) / count_raw * 100, 1))
@@ -115,8 +153,9 @@ def main():
         print('\nExiting gracefully...')
     finally:
         termios.tcsetattr(stdin_fd, termios.TCSADRAIN, old_settings)
-        stdout(codes['cursor_visible'])
+        print(codes['cursor_visible'])
     return main() if input().lower() == 'y' else None
+
 
 if __name__ == '__main__':
     main()
